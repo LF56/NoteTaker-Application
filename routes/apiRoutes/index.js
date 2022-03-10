a911119bde7655d379ec06f7e8bb696288286569
LF56 <leah.fox7@gmail.com>
2022-03-10 08:15:04 -0500

    added module export

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,25 +3,15 @@ const fs = require('fs')
 const path = require('path')  
 
 
-
 router.get('/notes', (req, res) => {
       
-fs.readFile('db/db.json','utf8', function(err,data){
-    if (!err) {
-       throw (err)
-    } else {
-        console.log(data);
-        res.json(data)
-    }
-});
-})
-
-
-
-
-
-
-
-
-
+    fs.readFile('db/db.json','utf8', function(err,data){
+        if (!err) {
+           throw (err)
+        } else {
+            console.log(data);
+            res.json(data)
+        }
+    });
+    })
 module.exports = router;
